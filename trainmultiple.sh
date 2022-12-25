266ea517157da6abd9d82f85b2daa2ae5cb891b9
SuperSecureHuman <supersecurehuman@gmail.com>
2022-12-25 10:33:22 +0530

    Minor Changes

--- trainmultiple.sh
+++ trainmultiple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python train.py --learning_rate=0.0005 --batch_size=16 --epochs=30 --model=SKNet$1 --wandb=True --data=$2
-python train.py --learning_rate=0.0001 --batch_size=16 --epochs=30 --model=SKNet$1 --wandb=True --data=$2
-python train.py --learning_rate=0.001 --batch_size=16 --epochs=30 --model=SKNet$1 --wandb=True --data=$2
+python train.py --learning_rate=0.0005 --batch_size=16 --epochs=20 --model=SKNet$1 --wandb=True --data=$2
+python train.py --learning_rate=0.0001 --batch_size=16 --epochs=20 --model=SKNet$1 --wandb=True --data=$2
+python train.py --learning_rate=0.001 --batch_size=16 --epochs=20 --model=SKNet$1 --wandb=True --data=$2
